7a53fc0576bf08bbba9a815bcccb8cb29200b1cf
Uhlric-W <uhlricwymer@gmail.com>
2026-01-08 14:23:13 -0600

    completed header file for transactions and created getters for transactions

--- code/include/Transaction.h
+++ code/include/Transaction.h
@@ -6,7 +6,37 @@
 using namespace std;
 
 class Transaction {
+    public:
+     Transaction();
     
+     //getters and setters for each variable
+     double get_amount();
+
+     time_t get_date();
+
+     string get_category();
+
+     int get_id();
+
+     string get_description();
+
+     void set_id(int new_id);
+
+     void set_date(time_t new_date);
+
+     void set_amount(double new_amount);
+
+     void set_category(string new_category);
+     
+     //creates human readable string of all of the information contained in the transaction
+     string summarize();
+
+    private:
+     double amount;
+     time_t date;
+     string category; // label to sort expenses and transactions by types of transactions
+     int id;
+     string description;
 };
 
 #endif
---
+++ code/src/Transaction.cpp
@@ -0,0 +1,21 @@
+#include "Transaction.h"
+
+int get_id(){
+    return id;
+}
+
+double get_amount(){
+    return amount;
+}
+
+string get_category(){
+    return category;
+}
+
+time_t get_date(){
+    return date;
+}
+
+string get_description(){
+    return description;
+}
